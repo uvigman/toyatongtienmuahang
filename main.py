8e8a2636765ac71911abbd81697b457fb3bc68ac
Nam G VU <namgivu@gmail.com>
2023-10-17 13:32:25 +0700

    update chambai

---
+++ main.py
@@ -0,0 +1,39 @@
+from s00_bailam import hi as bailam_f
+
+
+#region chambai
+from s02_chambai import chambai
+
+testcase_list = [
+    {'tc_name': 'tc0', 'input': {'name':'AI BTX'}, 'output':'Hi AI BTX'},  
+    {'tc_name': 'tc1', 'input': {'name':'HSU'},    'output':'Hi'},    
+    {'tc_name': 'tc2', 'input': {'name':None},     'output':'Hi'},
+    {'tc_name': 'tc3', 'input': {'name':''},       'output':'Hi AI BTX'},  
+]
+
+ketqua_list = []
+for tc in testcase_list:  # tc aka testcase
+  INP_name = tc['input']
+  tc_score, o_BAILAM = chambai(tc, bailam_f)
+  
+  ketqua_list.append({
+    'tc_name'    : tc['tc_name'],
+    'tc_score'   : tc_score,  
+    'o_TESTCASE' : tc['output'],    
+    'o_BAILAM'   : o_BAILAM,  
+  })
+#endregion chambai
+
+#region in ketquqa
+print('---ketqua chitiet')
+for kq in ketqua_list:
+  print(f'''
+{kq['tc_name']} {kq['tc_score']}
+o_TESTCASE = {kq['o_TESTCASE']}
+o_BAILAM   = {kq['o_BAILAM']}
+  '''.strip()+'\n')
+
+print('\n---ketqua')
+for kq in ketqua_list:
+  print(f'''{kq['tc_name']} {kq['tc_score']}''')
+#endregion in ketquqa
---
+++ s00_bailam.py
@@ -0,0 +1,17 @@
+"""
+--- debai 
+Viet ham 
+def hi(name):
+  # tra ve cau chao 'Hi {name}!'
+
+--- vidu
+Khi chay voi input | Ketqua output
+------------------ | ------------
+hi('AI-BTX')       | Hi AI-BTX!
+hi('HSU')          | Hi HSU!
+hi(None)           | Hi!
+hi('')             | Hi!
+"""
+
+def hi(name):
+  return 'TODO'
